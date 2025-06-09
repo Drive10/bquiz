18b3bf546e7d3290fa5df951cead54ea24b668e0
Umesh Lohot <umeshlohot@gmail.com>
2025-06-09 17:30:44 +0530

    Add remain class

---
+++ src/main/java/com/example/backendquiz/BackendquizApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendquiz/BackendquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
